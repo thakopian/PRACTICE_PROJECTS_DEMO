4fba9a679d3791acd181d624ce8c1845a2ba8507
Tadeh Hakopian <thakopian@gmail.com>
2022-02-24 07:57:24 -0800

    Initial commit

---
+++ ENR_TYP_HTML_01/TEST_01/src/index.js
@@ -0,0 +1,20 @@
+// The above example is just about the most bare and basic html page you can create.
+// Look at the panel in the lower right corner of this jsFiddle page, and you'll see the output
+// of our first getting-started html page. All the text you see in the html panel above is commonly
+// referred to as "html" or "html markup".
+
+// Each pair of "tags" (i.e. <body> and </body>) have a beginning (<body>) and an ending (</body>).
+// This special pattern of "markup" code instructs the web browser how to display the page to the user is a structured
+// and visual way.
+
+// Tags explained:
+
+// <html>...</html> is the main wrapper around all of the page markup
+
+// <head>...<head>  content here is not visually displayed on when the page is rendered in the browser,
+//                  but it contains important information about the page that the web browser
+//                  will use to render the page. Learn more in Lesson #XX.
+
+// <body>...</body> all the visible stuff that a user will see goes in the <body> section.
+
+// <script> ... </ script> Javascript code goes here which can added additional functionality to the web page.
